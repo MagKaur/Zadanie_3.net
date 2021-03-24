9f16be54c08336db1cb5cdcf101cf035fae6ab13
MagKaur <kochanowska.magd@gmail.com>
2021-03-25 00:46:39 +0100

    Professor version

---
+++ ConsoleApp2/ChangeHour.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Akcesory
+{
+    class Hours
+    {
+        private int hour; // ta zmienna posiada właściwość
+        private int second;
+        
+
+        public int Hour // Definicja właściwość
+        {
+            get { return hour; }
+            set
+            {
+                if (value >= 0 && value <= 24)
+                {
+                    hour = value;
+                    second = value * 3600;
+                }
+            }
+        }
+
+        public string Info()
+        {
+            return "Godzina " + hour + ". " + second +
+                   "  sekunda tej doby.";
+        }
+    }
+}
--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -6,7 +6,15 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            Hours godz = new Hours();
+            godz.Hour = 3; //dostęp do zmiennej prywatnej poprzez właściwość
+            //przypisanie możliwe dzięki sekcji set we własności Hour
+            Console.WriteLine(godz.Info());
+            //sekcja set nie tylko umożliwia bezpośredni dostęp do zmiennej 
+            //prywatnej hour, ale sprawuje kontrolę nad jej zakresem i 
+            //pozwala ustalić wartości innych zmiennych prywatnych
+            Console.ReadKey();
         }
     }
 }
